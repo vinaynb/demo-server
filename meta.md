85e97243b2d01c81caf530fff8126b133c794e95
vinay <vinaynb@gmail.com>
2016-12-28 18:20:37 +0530

    moved git repo

---
+++ meta.md
@@ -0,0 +1,5 @@
+# to add currently running processes to the startup script
+`pm2 save`
+
+# path where pm2 dumps info about currently running processes that are captured by startup script
+`/home/upc1/.pm2/dump.pm2`
---
+++ server-1/ecosystem.config.js
@@ -0,0 +1,52 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // First application
+    {
+      name      : "Server 1",
+      script    : "index.js",
+      env: {
+        COMMON_VARIABLE: "true"
+      },
+      env_production : {
+        NODE_ENV: "production"
+      }
+    },
+
+    // Second application
+    {
+      name      : "Server 2",
+      script    : "../server-2/index.js"
+    }
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      user : "ubuntu",
+      host : ["54.169.50.230","52.77.209.244"],
+      ref  : "origin/master",
+      repo : "https://github.com/vinaynb/demo-server.git",
+      path : "/home/ubuntu/Desktop/pm2Test",
+      "post-deploy" : "pm2 startOrRestart ecosystem.config.js --env production"
+    },
+    dev : {
+      user : "node",
+      host : "212.83.163.1",
+      ref  : "origin/master",
+      repo : "git@github.com:repo.git",
+      path : "/var/www/development",
+      "post-deploy" : "npm install && pm2 startOrRestart ecosystem.json --env dev",
+      env  : {
+        NODE_ENV: "dev"
+      }
+    }
+  }
+}

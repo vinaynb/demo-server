d666921cb90a90e90706e23221874433ef69220d
vinay <vinaynb@gmail.com>
2016-12-28 18:00:38 +0530

    ecosystem changes for aws

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const http = require('http')  
+const http = require('http')
 const port = 3000
 
-const requestHandler = (request, response) => {  
+const requestHandler = (request, response) => {
   console.log(request.url)
-  response.end('Hello Node.js Server on 3000 port!')
+  response.end('Hello Node.js Server on 3000 port! WTF')
 }
 
 const server = http.createServer(requestHandler)
 
-server.listen(port, (err) => {  
+server.listen(port, (err) => {
   if (err) {
     return console.log('something bad happened', err)
   }
